3065920a435f15bee8107e4d2878e54881dd88a9
Nicolas Molina Monroy <hi@nicobytes.com>
2021-09-15 21:57:29 -0400

    :construction: Working in forms

--- src/components/forms/event.form.js
+++ src/components/forms/event.form.js
@@ -1,89 +1,77 @@
-import { Form, Input, Button, DatePicker, Select } from "antd";
+import { Form, Input, Button, DatePicker, Select } from 'antd';
 
 function EventForm() {
 
   const initialValues = {
-    title: "",
-    description: "",
-    startAt: "",
-    endAt: "",
-    type: "",
+    title: '',
+    description: '',
+    startAt: '',
+    endAt: '',
+    type: '',
   };
 
   const handleOnFinish = (values) => {
     console.log(values);
-  };
 
-  const handleOnFinishFailed = () => {};
+  }
 
   return (
     <Form
-      name="customer"
+      name="eventForm"
       initialValues={initialValues}
       onFinish={handleOnFinish}
-      onFinishFailed={handleOnFinishFailed}
-      autoComplete="off"
+      onFinishFailed
       labelCol={{
-        span: 3,
+        span: 3
       }}
-    >
-      <Form.Item
-        label="Name"
-        name="name"
-        rules={[
-          {
-            required: true,
-            message: "Name is mandatory!",
-          },
-        ]}
       >
+      <Form.Item label="Title" name="title" rules={[
+        {
+          required: true,
+          message: 'Title is mandatory'
+        }
+      ]}>
         <Input />
       </Form.Item>
-      <Form.Item
-        label="Description"
-        name="description"
-        rules={[
-          {
-            required: true,
-            message: "Address is mandatory!",
-          },
-        ]}
-      >
-        <Input.TextArea  />
+      <Form.Item label="Description" name="description" rules={[
+        {
+          required: true,
+          message: 'Description is mandatory'
+        }
+      ]}>
+        <Input.TextArea />
       </Form.Item>
-      <Form.Item label="Start Date" name="startAt">
+      <Form.Item label="Start Date" name="startAt" rules={[
+        {
+          required: true,
+          message: 'Start Date is mandatory'
+        }
+      ]}>
         <DatePicker />
       </Form.Item>
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Email is mandatory!"
-          },
-          {
-            type: "email",
-            message: "It's not valid email"
-          },
-        ]}
-      >
-        <Input />
+      <Form.Item label="End Date" name="endAt" rules={[
+        {
+          required: true,
+          message: 'End Date is mandatory'
+        }
+      ]}>
+        <DatePicker />
       </Form.Item>
-      <Form.Item label="Type" name="type">
+      <Form.Item label="Type" name="type" rules={[
+        {
+          required: true,
+          message: 'Type is mandatory'
+        }
+      ]}>
         <Select>
-          <Select.Option value="demo">Demo</Select.Option>
-          <Select.Option value="demo">Demo 1</Select.Option>
+          <Select.Option value="live">Live</Select.Option>
+          <Select.Option value="workshop">Workshop</Select.Option>
         </Select>
       </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          offset: 3,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Save
-        </Button>
+      <Form.Item wrapperCol={{
+        offset: 3
+      }}>
+        <Button type="primary" htmlType="submit"> Enviar </Button>
       </Form.Item>
     </Form>
   );
