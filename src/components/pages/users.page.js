427ba82d4509ae49388ab1f6eba306f0acfc0b12
Nicolas Molina Monroy <hi@nicobytes.com>
2021-09-29 17:37:22 -0400

    :construction: Update

--- src/components/pages/users.page.js
+++ src/components/pages/users.page.js
@@ -1,14 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { PageHeader, List, Card, Spin, Alert } from "antd";
-import useGetUsers from "./../../hooks/getUsers";
-import { GetUsers } from "./../../services/users.service";
+
 
 function UsersPage() {
-  const { data, isLoading, isError, isSuccess } = GetUsers();
+  const [data, setData] = useState([]);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [gender, setGender] = useState('female');
+
+  const getUsers = async() => {
+    console.log('getUsers', gender); // male
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`https://randomuser.me/api/?results=100&gender=${gender}`);
+      const data = response.data;
+      const results = data.results;
+      setData(results);
+      setIsLoading(false);
+      setIsSuccess(true);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  }
+
+  useEffect(()=> {
+    getUsers();
+  }, [gender]);
+
+  const changeGender = (value) => {
+    setGender(value);
+  }
 
   return (
     <div>
       <PageHeader title="User" subTitle="lista de usuarios" />
-
+      <h1>{gender}</h1>
+      <button onClick={() => changeGender('female')}>female</button>
+      <button onClick={() => changeGender('male')}>Male</button>
       { isLoading && <Spin /> }
       { isError &&  <Alert message="Error Text" type="error" /> }
       { isSuccess && <List
